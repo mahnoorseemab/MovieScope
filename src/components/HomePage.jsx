6d7b88818566fe5556891bc48cbf8a3d45e1e1ef
mahnoorseemab <mahnoorseemab81@gmail.com>
2026-07-28 20:44:42 +0500

    updates

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { useState } from 'react'
 
 const HomePage = () => {
+    const [searchInput, setSearchInput] = useState("")
+
     return (
         <div className='flex flex-col gap-5'>
             <Navbar />
@@ -10,10 +13,25 @@ const HomePage = () => {
                     className='p-3 w-[40vw] rounded-2xl bg-white/10 backdrop-blur-md border border-[#E63946]/40 focus:outline-none focus:border-[#E63946] focus:ring-2 focus:ring-[#E63946]/30 transition-all duration-300'
                     type="text"
                     placeholder='Search your favourite movie'
-                />
+                    value={searchInput}
+                    onChange={(e) => {
+                        setSearchInput(e.target.value)
+                    }}
+                    />
             </div>
         </div>
     )
 }
 
 export default HomePage
+
+
+// onKeyDown={
+//     () => {
+//         if (key == "Enter") {
+//           fetch()=>{
+//             .then()
+//           }
+//         }
+//     }
+// }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='bg-(--red) w-[100vw] h-[10vh]'>
+    <div className='bg-(--red) w-[100vw] h-[15vh]'>
         <div className='font-bold text-2xl'>
             MovieScope
         </div>
