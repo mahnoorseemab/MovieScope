03c076baf80265c37c4599e036afe225aa9e0f7a
mahnoorseemab <mahnoorseemab81@gmail.com>
2026-07-29 23:17:11 +0500

    updates

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -12,26 +12,29 @@ const HomePage = () => {
                 <input
                     className='p-3 w-[40vw] rounded-2xl bg-white/10 backdrop-blur-md border border-[#E63946]/40 focus:outline-none focus:border-[#E63946] focus:ring-2 focus:ring-[#E63946]/30 transition-all duration-300'
                     type="text"
-                    placeholder='Search your favourite movie'
+                    placeholder='Search your favourite movie here!'
                     value={searchInput}
                     onChange={(e) => {
                         setSearchInput(e.target.value)
                     }}
-                    />
+                    onKeyDown={
+                        (e) => {
+                            if (e.key === "Enter") {
+                                async function onhandlekey() {
+                                    const res = await fetch(`https://api.themoviedb.org/3/search/movie?query=${searchInput}&api_key=${import.meta.env.VITE_TMDB_API_KEY}`)
+                                    const data = await res.json()
+                                    console.log(data)
+                                }
+                                onhandlekey()
+                            }
+                        }
+                    }
+                />
             </div>
         </div>
     )
 }
 
+
 export default HomePage
 
-
-// onKeyDown={
-//     () => {
-//         if (key == "Enter") {
-//           fetch()=>{
-//             .then()
-//           }
-//         }
-//     }
-// }
