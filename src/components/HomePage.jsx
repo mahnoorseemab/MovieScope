4600ac452d9287bc4444bac8088ec1405c138204
mahnoorseemab <mahnoorseemab81@gmail.com>
2026-08-04 22:27:00 +0500

    updates

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -21,41 +21,48 @@ const HomePage = () => {
         getPopularMovies()
     }, [])
 
-    return (
-        <div className='flex flex-col gap-10'>
+    if (loadingState == true) {
+        return (
             <LoadingSpinner />
-            <Navbar />
-            <div className='flex justify-center'>
-                <input
-                    className='p-3 w-[40vw] rounded-2xl bg-white/10 backdrop-blur-md border border-[#E63946]/40 focus:outline-none focus:border-[#E63946] focus:ring-2 focus:ring-[#E63946]/30 transition-all duration-300'
-                    type="text"
-                    placeholder='Search your favourite movie here!'
-                    value={searchInput}
-                    onChange={(e) => {
-                        setSearchInput(e.target.value)
-                    }}
-                    onKeyDown={
-                        (e) => {
-                            if (e.key === "Enter") {
-                                async function onhandlekey() {
-                                    const res = await fetch(`https://api.themoviedb.org/3/search/movie?query=${searchInput}&api_key=${import.meta.env.VITE_TMDB_API_KEY}`)
-                                    const data = await res.json()
-                                    console.log(data)
+        )
+    }
+    else {
+        return (
+            <div className='flex flex-col gap-10'>
+                <Navbar />
+                <div className='flex justify-center'>
+                    <input
+                        className='p-3 w-[40vw] rounded-2xl bg-white/10 backdrop-blur-md border border-[#E63946]/40 focus:outline-none focus:border-[#E63946] focus:ring-2 focus:ring-[#E63946]/30 transition-all duration-300'
+                        type="text"
+                        placeholder='Search your favourite movie here!'
+                        value={searchInput}
+                        onChange={(e) => {
+                            setSearchInput(e.target.value)
+                        }}
+                        onKeyDown={
+                            (e) => {
+                                if (e.key === "Enter") {
+                                    async function onhandlekey() {
+                                        const res = await fetch(`https://api.themoviedb.org/3/search/movie?query=${searchInput}&api_key=${import.meta.env.VITE_TMDB_API_KEY}`)
+                                        const data = await res.json()
+                                        console.log(data)
+                                    }
+                                    onhandlekey()
                                 }
-                                onhandlekey()
                             }
                         }
-                    }
-                />
-            </div>
+                    />
+                </div>
 
-            <div className='flex justify-center font-bold text-3xl'>Popular Shows</div>
-            <div className='flex w-[100vw] border-2 h-[35vh]'>
-                {moviesArray.map((movies) => <MovieCard key={movies.id} movies={movies} />)}
+                <div className='flex justify-center font-bold text-3xl'>Popular Shows</div>
+                <div className='flex w-[100vw] border-2 h-[35vh]'>
+                    {moviesArray.map((movies) => <MovieCard key={movies.id} movies={movies} />)}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
 
 
 export default HomePage
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 
 const MovieCard = ({ key, movies }) => {
   return (
-    <div className="flex flex-col gap-2 border-2 w-[13vw] h-[55vh]">
-      <div className="border-2 w-[13vw] h-[45vh]"><img src={`https://image.tmdb.org/t/p/original${movies.poster_path}`} alt="" srcset="" /></div>
-      <div className="border-2 w-[13vw]">Title: {movies.title}</div>
-      <div className='border-2 w-[13vw]'>Release Date: {movies.release_date}</div>
+    <div className="flex flex-col gap-3 border-2 w-[11vw] h-[50vh]">
+      <div className="border-2 w-[11vw] h-[34vh]"><img src={`https://image.tmdb.org/t/p/original${movies.poster_path}`} alt="" srcSet="" /></div>
+      <div className="border-2 w-[11vw]">{movies.title}</div>
+      <div className='border-2 w-[11vw]'>Release Date: {movies.release_date}</div>
     </div>
   )
 }
