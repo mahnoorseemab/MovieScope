96c3b3ab5f1f865b382b46f6744a634e40ab3b8e
mahnoorseemab <mahnoorseemab81@gmail.com>
2026-08-17 00:15:05 +0500

    updates

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,10 +1,15 @@
 import SearchedDataCard from './SearchedDataCard'
+import { Link } from 'react-router-dom'
 
 const SearchResult = ({ searchedData }) => {
     return (
-        <div className='border-2'>
-            {searchedData.map((data) => <SearchedDataCard data={data} key={data.id} />)}
-            console.log(searchedData)
+        <div>
+            <div className='border-2'>
+                {searchedData.map((data) => <SearchedDataCard data={data} key={data.id} />)}
+            </div>
+            <div className='border-2 w-fit active:scale-95 transition-all hover:cursor-pointer'>
+                <Link to="/">Back to Home</Link>
+            </div>
         </div>
     )
 
