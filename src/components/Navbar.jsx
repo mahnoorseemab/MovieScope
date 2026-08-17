75024974a2b0413b0280904cc48f6ab63891ea9e
mahnoorseemab <mahnoorseemab81@gmail.com>
2026-08-17 12:48:50 +0500

    ui updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='flex bg-(--red) w-[100vw] h-[20vh] justify-around items-center'>
-      <div className='font-bold text-2xl'>
-        MovieScope
-      </div>
+      <div className='flex flex-col text-3xl font-extrabold tracking-widest uppercase text-black' style={{ textShadow: '1px 1px 3px rgba(0,0,0,0.3)' }}>
+        <span className='text-black'>Movie</span>
+        <span className='text-black-600'>Scope</span>
+      </div >
       <div className='flex gap-15 '>
         <span className='text-xl hover:text-(--white) hover:cursor-pointer'>
           <Link to="/">Home</Link>
@@ -15,7 +15,7 @@ const Navbar = () => {
           <Link to="/favourites">Favourites</Link>
         </span>
       </div>
-    </div>
+    </div >
   )
 }
 
