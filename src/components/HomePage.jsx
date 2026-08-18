b2c75c87216be0847a986454ae529e5082f834a8
mahnoorseemab <mahnoorseemab81@gmail.com>
2026-08-18 11:58:50 +0500

    updates

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -27,27 +27,29 @@ const HomePage = ({ handleAddToFavourites, loadingState, moviesArray, setSearche
         return (
             <div className='flex flex-col gap-10'>
                 <Navbar />
-                <div className='flex justify-center relative w-[fit]'>
-                    <input
-                        className='p-3 w-[40vw] rounded-2xl bg-white/10 backdrop-blur-md border border-[#E63946]/40 focus:outline-none focus:border-[#E63946] focus:ring-2 focus:ring-[#E63946]/30 transition-all duration-300'
-                        type="text"
-                        placeholder='Search movies here!'
-                        value={searchInput}
-                        onChange={(e) => {
-                            setSearchInput(e.target.value)
-                        }}
-                        onKeyDown={
-                            (e) => {
-                                if (e.key === "Enter") {
-                                    handleSearch()
+                <div className='flex justify-center'>
+                    <div className='flex relative w-[fit]'>
+                        <input
+                            className='p-3 w-[40vw] rounded-2xl bg-white/10 backdrop-blur-md border border-[#E63946]/40 focus:outline-none focus:border-[#E63946] focus:ring-2 focus:ring-[#E63946]/30 transition-all duration-300'
+                            type="text"
+                            placeholder='Search movies here!'
+                            value={searchInput}
+                            onChange={(e) => {
+                                setSearchInput(e.target.value)
+                            }}
+                            onKeyDown={
+                                (e) => {
+                                    if (e.key === "Enter") {
+                                        handleSearch()
+                                    }
                                 }
                             }
-                        }
-                    />
-                    <img src={searchIcon} className='w-7 h-7 absolute right-30 hover:cursor-pointer m-2.5' alt="" srcset=""
-                        onClick={() => {
-                            handleSearch()
-                        }} />
+                        />
+                        <img src={searchIcon} className='w-7 h-7  absolute right-3 hover:cursor-pointer m-2.5' alt="" srcset=""
+                            onClick={() => {
+                                handleSearch()
+                            }} />
+                    </div>
                 </div>
 
                 <div className='flex justify-center font-bold text-3xl'>Popular Shows</div>
