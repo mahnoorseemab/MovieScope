e668f84a836398413b7633273f2854a04ee8fed1
mahnoorseemab <mahnoorseemab81@gmail.com>
2026-08-21 18:24:24 +0500

    updates

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -8,29 +8,27 @@ const MovieDetail = ({ moviesArray }) => {
     <div>
       <div className='relative min-h-screen overflow-hidden'>
         <img className='absolute object-cover opacity-20 blur-sm inset-0 w-full' src={`https://image.tmdb.org/t/p/original${specificMovie.poster_path}`} alt="" />
-        <div className='flex'>
+        <div className='flex relative z-10'>
           <img className='h-screen p-3 rounded-2xl' src={`https://image.tmdb.org/t/p/original${specificMovie.poster_path}`} alt="" />
           <div className='flex flex-col'>
-
             <div className='flex items-baseline justify-between'>
-              <span className='text-3xl md:text-4xl font-bold text-black m-2'>{specificMovie.title}</span>
+              <span className='text-9xl md:text-4xl font-bold text-black m-4'>{specificMovie.title}</span>
               <div className='flex flex-col'>
                 <div className='flex items-center justify-center w-14 h-14 rounded-full border-5 border-(--red) bg-black/10'>
                   <span className='text-black font-bold text-sm'>{Math.round(specificMovie.popularity)}</span>
                 </div>
                 <span className='text-sm text-(--red)'>Popularity</span>
               </div>
-
             </div>
 
-            <span className='m-2'>{specificMovie.release_date}</span>
+            <span className='m-5'>{specificMovie.release_date}</span>
 
             <div className='flex items-center gap-4'>
-              <div className='flex items-center gap-1.5 bg-white/5 border border-white/10 rounded-full px-4 py-1.5'>
+              <div className='flex items-center gap-1.5  bg-red/5 border border-red/10 rounded-full px-4 py-1.5'>
                 <span className='text-(--red) font-bold text-sm'>{specificMovie.vote_average?.toFixed(1)}</span>
                 <span className='text-gray-400 text-xs'>/10</span>
               </div>
-              <div className='flex items-center gap-1.5 bg-white/5 border border-white/10 rounded-full px-4 py-1.5'>
+              <div className='flex items-center gap-1.5 bg-red/5 border border-red/10 rounded-full px-4 py-1.5'>
                 <span className='text-black font-semibold text-sm'>{specificMovie.vote_count}</span>
                 <span className='text-gray-400 text-xs'>votes</span>
               </div>
@@ -40,7 +38,6 @@ const MovieDetail = ({ moviesArray }) => {
               <h2 className='text-(--red) font-bold text-3xl mb-1'>Overview</h2>
               <p className='text-black text-lg leading-relaxed'>{specificMovie.overview}</p>
             </div>
-
 
           </div>
         </div>
