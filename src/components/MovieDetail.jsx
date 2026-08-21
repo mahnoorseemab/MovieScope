809fff23ff24003d1bcb156d597144d679666306
mahnoorseemab <mahnoorseemab81@gmail.com>
2026-08-21 16:40:15 +0500

    updates

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -7,16 +7,21 @@ const MovieDetail = ({ moviesArray }) => {
   return (
     <div>
       <div className='relative min-h-screen overflow-hidden'>
-        <img className='absolute object-cover opacity-70 blur-sm inset-0 w-full' src={`https://image.tmdb.org/t/p/original${specificMovie.poster_path}`} alt="" />
+        <img className='absolute object-cover opacity-20 blur-sm inset-0 w-full' src={`https://image.tmdb.org/t/p/original${specificMovie.poster_path}`} alt="" />
 
         <div className='flex'>
-          <img className='h-screen p-6' src={`https://image.tmdb.org/t/p/original${specificMovie.poster_path}`} alt="" />
-          <span>{specificMovie.title}</span>
-          <span>{specificMovie.popularity}</span>
-          <span>{specificMovie.release_date}</span>
-          <span>{specificMovie.overview}</span>
-          <span>{specificMovie.vote_average}</span>
-          <span>{specificMovie.vote_count}</span>
+          <img className='h-screen p-3 rounded-2xl' src={`https://image.tmdb.org/t/p/original${specificMovie.poster_path}`} alt="" />
+          <div className='flex flex-col'>
+            <span className='text-3xl md:text-4xl font-bold text-black m-2'>{specificMovie.title}</span>
+            <span className='m-2'>{specificMovie.release_date}</span>
+            <div className='mt-2 max-w-3xl'>
+              <h2 className='text-(--red) font-bold text-lg mb-1'>Overview</h2>
+              <p className='text-black text-sm leading-relaxed'>{specificMovie.overview}</p>
+            </div>
+            <span>{specificMovie.popularity}</span>
+            <span>{specificMovie.vote_average}</span>
+            <span>{specificMovie.vote_count}</span>
+          </div>
         </div>
       </div>
     </div>
