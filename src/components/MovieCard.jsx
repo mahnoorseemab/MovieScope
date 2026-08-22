6f68c698d4bfa2dd7e7682d23d4db5a3adf765fc
mahnoorseemab <mahnoorseemab81@gmail.com>
2026-08-22 21:39:36 +0500

    updates

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,17 +7,32 @@ const MovieCard = ({ movie, handleAddToFavourites }) => {
 
   return (
     <div>
-      <div className="flex flex-col w-[12vw] h-[50vh]">
+      <div className="flex flex-col w-[12vw] min-w-[160px] gap-2 rounded-xl p-2 shadow-md hover:shadow-lg
+            hover:scale-95 transition-all duration-200 bg-white">
         <div className='hover:cursor-pointer' onClick={() => {
           navigate(`/moviedetail/${movie.id}`)
         }}>
-          <div className="flex rounded-xl h-[34vh]"><img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt="" /></div>
-          <div className="font-bold hover:underline flex-wrap">{movie.title}</div>
+          <div className="flex rounded-xl overflow-hidden aspect-[2/3]">
+            <img
+              className='w-full h-full object-cover'
+              src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+              alt={movie.title}
+            />
+          </div>
+          <div className="font-semibold text-sm mt-2 hover:underline line-clamp-2 leading-snug">
+            {movie.title}
+          </div>
         </div>
-        <div>{movie.release_date}</div>
+
+        <div className='text-xs text-gray-500'>{movie.release_date}</div>
+
         <div className='flex gap-3'>
           <span>
-            <img className='hover:cursor-pointer' title='Add to Favourites' src={addToFavourites} alt=""
+            <img
+              className='hover:cursor-pointer w-5 h-5'
+              title='Add to Favourites'
+              src={addToFavourites}
+              alt="Add to favourites"
               onClick={() => {
                 handleAddToFavourites(movie)
                 navigate('/favourites')
@@ -28,6 +43,7 @@ const MovieCard = ({ movie, handleAddToFavourites }) => {
       </div>
     </div>
   )
+
 }
 
 export default MovieCard
