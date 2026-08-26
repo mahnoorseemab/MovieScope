ef8dcdbb969f8bdb0f5769b0f012cae311cee0a7
mahnoorseemab <mahnoorseemab81@gmail.com>
2026-08-26 23:20:49 +0500

    updated favouries page

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -2,17 +2,32 @@ import FavouritesCard from './FavouritesCard'
 import BackButton from './BackButton'
 
 const Favourites = ({ favourites }) => {
-  
-  return (
-    <div className="flex flex-col p-4">
-      <div className='flex flex-wrap'>
-        {favourites.map((favourite) => <FavouritesCard favourite={favourite} key={favourite.id} />)}
-      </div>
-      <div className='w-fit mt-3'>
+  if (favourites.length == 0) {
+    return (
+      <div className='flex flex-col justify-center items-center h-[100vh] gap-3'>
+        <span className='text-6xl'>🎬</span>
+        <h2 className='text-2xl font-bold tracking-wide text-black'>
+          Nothing here yet — add a movie you love
+        </h2>
+        <p className='text-sm text-gray-500'>
+          Go back to home and add start your watchlist
+        </p>
         <BackButton />
       </div>
-    </div>
-  )
+    )
+  } else {
+
+    return (
+      <div className="flex flex-col p-4">
+        <div className='flex flex-wrap'>
+          {favourites.map((favourite) => <FavouritesCard favourite={favourite} key={favourite.id} />)}
+        </div>
+        <div className='w-fit mt-3'>
+          <BackButton />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Favourites
